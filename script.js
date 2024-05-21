090a28dc5def4668ca7eb616cc42570317efccd6
´´Sozky19´´ <elribiusmari@gmail.com>
2024-05-21 02:24:06 -0600

    Pagina Web Random

---
+++ script.js
@@ -0,0 +1,18 @@
+// script.js
+
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.style.display = i === index ? 'block' : 'none';
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+setInterval(nextSlide, 3000); // Cambiar imagen cada 3 segundos
+showSlide(currentSlide);
